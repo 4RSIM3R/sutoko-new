c17f67582e3c03882db15397e693d7f1f2eb5612
unknown <ilzammulkhaq85@gmail.com>
2024-11-12 21:08:24 +0700

    begin develop anamesis and encounter

--- resources/js/pages/backoffice/operational/encounter/form.tsx
+++ resources/js/pages/backoffice/operational/encounter/form.tsx
@@ -1,7 +1,13 @@
 import { AppLayout } from "@/layouts/app-layout";
 
 export default function EncounterForm() {
-    
+
+    return (
+        <div>
+
+        </div>
+    )
+
 }
 
 EncounterForm.layout = (page: any) => <AppLayout children={page} />;
--- resources/js/pages/backoffice/operational/encounter/index.tsx
+++ resources/js/pages/backoffice/operational/encounter/index.tsx
@@ -2,6 +2,12 @@ import { AppLayout } from "@/layouts/app-layout";
 
 export default function EncounterIndex() {
     
+    return (
+        <div>
+            
+        </div>
+    )
+
 }
 
 EncounterIndex.layout = (page: any) => <AppLayout children={page} />;
