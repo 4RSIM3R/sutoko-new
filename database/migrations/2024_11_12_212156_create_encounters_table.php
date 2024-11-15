8ef52652e828d5d2ec34601614ae0d26bf458637
unknown <ilzammulkhaq85@gmail.com>
2024-11-15 21:20:56 +0700

    anamnesis first i think

--- database/migrations/2024_11_12_212156_create_encounters_table.php
+++ database/migrations/2024_11_12_212156_create_encounters_table.php
@@ -24,10 +24,8 @@ return new class extends Migration
             $table->string('satu_sehat_id');
             $table->string('status');
             $table->boolean('send_questionnaire')->default(false);
-            $table->json('request')->nullable();
             $table->json('response')->nullable();
             $table->timestamps();
-            $table->dateTime('synced_at')->nullable();
             $table->softDeletes();
             $table->index(['patient_id', 'practioner_id', 'payment_assurance_id']);
         });
---
+++ database/migrations/2024_11_15_211541_create_anamneses_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Models\Encounter;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('anamneses', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Encounter::class)->constrained()->cascadeOnDelete()->cascadeOnUpdate();
+            $table->enum('type', ['primary', 'secondary']);
+            $table->string('notes');
+            $table->string('snomed_code');
+            $table->string('snomed_display');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('anamneses');
+    }
+};
