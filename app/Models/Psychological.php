7934414d5476c73a2a4c2a9b876390c499507e2f
unknown <ilzammulkhaq85@gmail.com>
2024-11-19 13:42:38 +0700

    develop diagnose first i think

---
+++ app/Models/Psychological.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Psychological extends Model
+{
+    /** @use HasFactory<\Database\Factories\PsychologicalFactory> */
+    use HasFactory;
+}
